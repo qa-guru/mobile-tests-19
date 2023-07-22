9a34d2604f7ad85c6584f399941cce4954fa781e
svasenkov <aanher@gmail.com>
2023-07-22 16:25:38 +0200

    added selenide local appium test

--- src/test/java/tests/browserstack/browserstack_sample/SearchTests.java
+++ src/test/java/tests/browserstack/browserstack_sample/SearchTests.java
@@ -1,4 +1,4 @@
-package tests.browserstack_sample;
+package tests.browserstack.browserstack_sample;
 
 //import io.appium.java_client.MobileBy;
 //import io.appium.java_client.android.AndroidDriver;
---
+++ src/test/java/tests/local/TestBase.java
@@ -0,0 +1,40 @@
+package tests.local;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import drivers.BrowserstackDriver;
+import drivers.LocalAndroidDriver;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class TestBase {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browser = LocalAndroidDriver.class.getName();
+        Configuration.browserSize = null;
+    }
+
+    @BeforeEach
+    void addListener() {
+//        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        open();
+    }
+
+    @AfterEach
+    void afterEach() {
+        String sessionId = sessionId().toString();
+        System.out.println(sessionId);
+
+//        Attach.screenshotAs("Last screenshot"); // todo
+        Attach.pageSource();
+
+        closeWebDriver();
+
+        Attach.addVideo(sessionId);
+    }
+}
